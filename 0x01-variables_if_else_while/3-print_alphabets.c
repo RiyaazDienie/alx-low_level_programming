ea9be8436d83c08484e25532840f6543b4eff079
RiyaazDienie <117740112+RiyaazDienie@users.noreply.github.com>
2022-12-09 18:20:59 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,29 +1,33 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include <stdlib.h>
 
 /**
- * main - Entry point
- * Description: Print lower and upper alphabet on the same line
- * Return: Always 0 (Success)
- * Author: IanoNjuguna
- */
-
+ * main - Prints alphabet
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints the alphabet in lower and upper case
+ * Can only use putchar
+ *
+ * Return: 0 for success
+*/
 int main(void)
 {
-char z = 'a';
-char Z = 'A';
+	char letter = 'a';
 
-while (z <= 'z')
-{
-	putchar(z);
-	z++;
-}
-while (Z <= 'Z')
-{
-	putchar(Z);
-	Z++;
-}
-putchar('\n');
-return (0);
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	letter = 'A';
+
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
 }
