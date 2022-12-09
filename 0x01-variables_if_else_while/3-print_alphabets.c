e83a1413d62294736fc1c9b8d90e414e5b52f624
RiyaazDienie <117740112+RiyaazDienie@users.noreply.github.com>
2022-12-09 18:29:02 +0200

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Entry point
- * Description: Print lower and upper alphabet on the same line
+ * Description: Print lowercase alphabet excluding e and q
  * Return: Always 0 (Success)
  * Author: IanoNjuguna
  */
@@ -12,17 +12,14 @@
 int main(void)
 {
 char z = 'a';
-char Z = 'A';
 
 while (z <= 'z')
 {
-	putchar(z);
+	if (z != 'e' && z != 'q')
+	{
+		putchar(z);
+	}
 	z++;
-}
-while (Z <= 'Z')
-{
-	putchar(Z);
-	Z++;
 }
 putchar('\n');
 return (0);
